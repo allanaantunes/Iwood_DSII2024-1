3c59ca43d39c21e52333971765a3745298cc8b03
kaue <kaue.nagiferreira@gmail.com>
2024-08-28 16:24:16 -0300

    add fix

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SugestaoController;
-use App\Http\Controllers\EstabelecimentoController;
-use App\Http\Controllers\ServicoController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\OrcamentoController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\UsuarioController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/estabelecimento/search', [EstabelecimentoController::class, "search"])->name('estabelecimento.search');
-Route::get('/estabelecimento/chart', [EstabelecimentoController::class, 'chart'])->name('estabelecimento.chart');
-Route::get('/estabelecimento/PDFEstabelecimento/', [EstabelecimentoController::class, "PDFEstabelecimento"])->name('estabelecimento.PDFEstabelecimento');
-Route::resource('estabelecimento', EstabelecimentoController::class);
-
-Route::post('/servico/search', [ServicoController::class, "search"])->name('servico.search');
-Route::get('/servico/chart', [ServicoController::class, 'chart'])->name('servico.chart');
-Route::resource('servico', ServicoController::class);
-
-Route::post('/orcamento/search', [OrcamentoController::class, "search"])->name('orcamento.search');
-Route::get('/orcamento/PDFOrcamento/', [OrcamentoController::class, "PDFOrcamento"])->name('orcamento.PDFOrcamento');
-Route::resource('orcamento', OrcamentoController::class);
-
-Route::post('/sugestao/search', [SugestaoController::class, "search"])->name('sugestao.search');
-Route::resource('sugestao', SugestaoController::class);
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__ . '/auth.php';
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::post('/pess/usuario', [UsuarioController::class, 'pess'])->name('pess.usuario');
-Route::resource('usuario', UsuarioController::class);
+require __DIR__.'/auth.php';
